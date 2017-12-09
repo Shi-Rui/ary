da6f77206c6bff57c7a8419ab0713c42a1b34853
shirui <1229408499@qq.com>
2017-12-09 15:44:42 +0800

    加入了一个任意进制转任意进制的函数，并且删掉了多余代码。

--- D2B.scm
+++ D2B.scm
@@ -9,7 +9,7 @@
   (if (null? l)
       (newline)
       (begin
-        (display (car l))
+        (display (number2H (car l)))
         (display-list (cdr l)))))
 
 (define (display-all in)
@@ -39,30 +39,6 @@
         [(or (equal? h "f") (equal? h "F")) 15]
         [else (H2number (string->number h))]))
 
-
-
-(define (D2B d)
-  (define (iter d l)
-    (if (= (f2i (/ d 2)) 0)
-        (cons (modulo d 2) l)
-        (iter (f2i (/ d 2)) (cons (modulo d 2) l))))
-  (iter d '()))
-
-(define (D2* d a)
-  (define (iter d l)
-    (if (= (f2i (/ d a)) 0)
-        (cons (modulo d a) l)
-        (iter (f2i (/ d a)) (cons (modulo d a) l))))
-  (iter d '()))
-
-(define (D2O d)
-  (D2* d 8))
-
-(define (D2H d)
-  (map number2H (D2* d 16)))
-
-
-
 (define (low-num n)
   (cond [(number? n) (f2i (/ n 10))]
         [(string? n) (substring n 0 (sub1 (string-length n)))]
@@ -75,12 +51,14 @@
         [(symbol? n) (get-last-num (symbol->string n))]
         [else #f]))
 
-(define (B2D b)
-  (define (iter b d c)
-    (if (= b 0)
-        d
-        (iter (low-num b) (+ d (* (get-last-num b) (expt 2 c))) (add1 c))))
-  (iter b 0 0))
+
+
+(define (D2* d a)
+  (define (iter d l)
+    (if (= (f2i (/ d a)) 0)
+        (cons (modulo d a) l)
+        (iter (f2i (/ d a)) (cons (modulo d a) l))))
+  (iter d '()))
 
 (define (*2D n a)
   (define (iter n d c)
@@ -92,8 +70,8 @@
          (add1 c))))
   (iter n 0 0))
 
-
-
-(define num (read))
-(newline)
-(display-all (*2D num 16))
+(define (*2* in out num)
+  (cond [(= in 10) (D2* num out)]
+        [(= out 10) (*2D num in)]
+        [(= in out) num]
+        [else (D2* (*2D num in) out)]))
